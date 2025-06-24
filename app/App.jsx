5214858630e5101192d1f34e8a735ad73fd22dd4
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-06-24 23:34:37 +0000

    Hello world

---
+++ app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, world!</h1>
+      <p>Welcome to your Azure Static Web App deployed with GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
